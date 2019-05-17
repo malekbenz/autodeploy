5069cefae74f52011ef1d22eea52501154b7e021
Malekbenz <malekbenz@gmail.com>
2019-05-17 19:35:16 +0100

    fix error

--- server.js
+++ server.js
@@ -7,12 +7,55 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 
 
-app.post('/deploy/red/', function (req, res) {
+app.use(express.static('public'));
+
+app.use('/', function (req, res, next) {
+     req.body.event = req.headers['x-github-event'];
+
+     if(req.body.ref){
+ 		req.body.branch = req.body.ref.split('/')[2];
+ 	}
+     next();
+ });
+
+app.use('/', function (req, res, next) {
+	
+	console.log("req.url", req.url);
+    if (req.body.event != 'push') {
+        res.statuscode = 200
+        res.end('not a push event ' + req.url);
+
+        return;
+    }
+
+    // exec('deploy.bat', function (error, stdout, stderr) {
+        // console.log('build has is triggered on ', req.body.branch);
+        // console.log(stdout);
+        // if (error != null) {
+            // console.log('error during the execution of redeploy: ' + stderr);
+            // res.statuscode = 200
+            // res.end('error during the execution of redeploy');
+            // return;
+        // }
+    // });
+    next();
+});
+
+ 
+app.post('/', function (req, res) {
 	
 	var response = { success : false, message: '' };
+    if (req.body.branch != 'develop' && req.body.branch != 'master') {
+
+        console.log('a %s event on %s branch', req.body.event , req.body.branch);
+        res.statuscode = 200;
+        res.end('not a master branch');
+        return;
+    }
+
 	exec('deploy.bat', function (error, stdout, stderr) {
 			console.log('build has is triggered on Develop ');
 			console.log(stdout);
@@ -36,45 +79,8 @@ app.get('/deploy', function (req, res) {
 });
 
 
-// app.use('/', function (req, res, next) {
-//     req.body.event = req.headers['x-github-event'];
 
-//     if(req.body.ref){
-// 		req.body.branch = req.body.ref.split('/')[2];
-// 	}
-//     next();
-// });
-
-// app.use('/', function (req, res, next) {
-	
-// 	console.log("req.url", req.url);
-//     if (req.body.event != 'push') {
-//         res.statusCode = 200
-//         res.end('Not a push event ' + req.url);
-
-//         return;
-//     }
-//     if (req.body.branch != 'develop') {
-
-//         console.log('a %s event on %s branch', req.body.event , req.body.branch);
-//         res.statusCode = 200;
-//         res.end('Not a master branch');
-//         return;
-//     }
-
-//     exec('deploy.bat', function (error, stdout, stderr) {
-//         console.log('build has is triggered on ', req.body.branch);
-//         console.log(stdout);
-//         if (error != null) {
-//             console.log('Error during the execution of redeploy: ' + stderr);
-//             res.statusCode = 200
-//             res.end('Error during the execution of redeploy');
-//             return;
-//         }
-//     });
-//     next();
-// });
-
+ 
 
 app.listen(PORT, function () {
     console.log('Server listening on port ', PORT )
